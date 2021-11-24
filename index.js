768e25cd9360c30253f0c5dd80493157372d07ec
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-23 21:00:31 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(pickup){
+    return Math.abs(pickup - hq);
+}
+
+function distanceFromHqInFeet(pickup){
+  return distanceFromHqInBlocks(pickup) * 264;
+}
+
+
+function distanceTravelledInFeet(hq, pickup) {
+return Math.abs((pickup - hq) * 264)
+}
+
+function calculatesFarePrice(hq, pickup) {
+  let fare = (distanceTravelledInFeet(pickup, hq))
+  if (fare <= 400) {
+    return 0;
+
+  } else if (fare > 2500){
+    return 'cannot travel that far'
+
+  } else if (fare > 2000){
+    return 25;
+
+  } else if (fare > 400){
+    return (fare - 400) * 0.02
+
+  }
+
+}
